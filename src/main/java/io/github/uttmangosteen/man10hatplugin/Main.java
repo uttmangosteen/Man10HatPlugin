e8ba6c9a4e5f37ab7414200e3578d0d00591653e
Underthetree <noisimportantpoint@gmail.com>
2025-01-24 16:28:15 +0900

    Changes

--- src/main/java/io/github/uttmangosteen/man10hatplugin/Main.java
+++ src/main/java/io/github/uttmangosteen/man10hatplugin/Main.java
@@ -3,9 +3,11 @@ package io.github.uttmangosteen.man10hatplugin;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class Main extends JavaPlugin {
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(new Event(), this);
-        getCommand("mhat").setExecutor(new Command());
+        Objects.requireNonNull(getCommand("mhat")).setExecutor(new Command());
     }
 }
